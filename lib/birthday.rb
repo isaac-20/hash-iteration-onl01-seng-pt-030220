f96dfef5459cba7da90f5dac61cc819879214ae3
Isaac Smith <isaacsmith215@aol.com>
2020-03-18 20:41:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,10 +6,15 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age < 13
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else
+      return false
+    end
   end
 end
 
+<<<<<<< HEAD
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 13
@@ -18,4 +23,7 @@ def age_appropriate_birthday(birthday_kids)
       return false
     end
   end
+=======
+def age_appropriate_birthday
+>>>>>>> ee95c789e05bcbd13833ae91cf4abe7fac2d2850
 end
